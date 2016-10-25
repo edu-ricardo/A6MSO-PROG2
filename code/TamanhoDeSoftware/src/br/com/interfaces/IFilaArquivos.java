4bd90e9cc38f6155d4bc1ee7515b46d625050db7
edu-ricardo <edu_ricardo@live.com>
2016-10-24 23:35:37 -0200

    Init dev

---
+++ code/TamanhoDeSoftware/src/br/com/interfaces/IFilaArquivos.java
@@ -0,0 +1,5 @@
+package br.com.interfaces;
+
+public interface IFilaArquivos {
+	public void carregaArquivos(String path);
+}
---
+++ code/TamanhoDeSoftware/src/br/com/interfaces/TipoImpressao.java
@@ -0,0 +1,6 @@
+package br.com.interfaces;
+
+public enum TipoImpressao {
+	tiTela,
+	tiTxt
+}
